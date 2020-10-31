c28d9794ce489e1cf5a54243cfd2bf1d7af17b33
kirall36 <43152550+kirall36@users.noreply.github.com>
2020-10-31 03:13:28 +0300

    Revert "Add authentication by jwt token"

--- backend/src/main/java/ru/jun/al/haz/homeless/animal/service/RoleService.java
+++
@@ -1,8 +0,0 @@
-package ru.jun.al.haz.homeless.animal.service;
-
-import ru.jun.al.haz.homeless.animal.service.dto.RoleDTO;
-
-public interface RoleService {
-
-    RoleDTO findByName(String name);
-}
--- backend/src/main/java/ru/jun/al/haz/homeless/animal/service/dto/AuthenticationResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package ru.jun.al.haz.homeless.animal.service.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthenticationResponseDTO {
-    private String login;
-    private String token;
-}
